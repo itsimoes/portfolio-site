ae99f2f82ffc017ad62c2f8f7afe6d24df0a69b9
Italo Simões <2d.italo@gmail.com>
2023-01-27 09:26:43 -0300

    transição entre componentes do formulario

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,11 +12,11 @@ const Contact = () => {
     const formComponents = [<UserForm />, <Thanks />];
     const [currentStep, setCurrentStep] = useState(formComponents[0]);       
 
-    const changeStep = (e, button) => {
+    const changeStep = (e, el) => {
         e.preventDefault()        
-        setCurrentStep(formComponents[1])
-      
-        // console.log({ name, email, message, currentStep })
+        setCurrentStep(formComponents[1])        
+        document.getElementById(el).style.display = 'none';
+        // console.log({el, disp})
     }
 
 
@@ -25,9 +25,10 @@ const Contact = () => {
             <h1 className='title'>Vamos conversar.</h1>
             <p className='contact-p'>Entre em contato preenchendo o formulário a seguir ou pelo email <a href="mailto:itsimoes1@gmail.com?subject=Olá!" class="links">itsimoes1@gmail.com</a>.</p>
             <div className='form-wrapper'>
-                <form id='contact-form' action='submit' onSubmit={(e) => changeStep(e)}>
+                <form id='contact-form' action='submit' onSubmit={(e) => changeStep(e, 'contact-button')}>
                     {currentStep}
                     <button
+                        id='contact-button'
                         className='contact-button'
                         type='submit'
                         form='contact-form'
