4a78894653808f0d90ac2b51a50f613c97070417
Italoe <2d.italo@gmail.com>
2023-01-27 08:42:38 -0300

    formulario criado

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,22 +1,38 @@
-import { TextField } from '@mui/material'
 import { Container } from '@mui/system'
 import React from 'react'
 import './style.css'
+import { useState } from 'react'
+
+//Form components
+import UserForm from './UserForm'
+import Thanks from './Thanks'
 
 const Contact = () => {
+
+    const formComponents = [<UserForm />, <Thanks />];
+    const [currentStep, setCurrentStep] = useState(formComponents[0]);       
+
+    const changeStep = (e, button) => {
+        e.preventDefault()        
+        setCurrentStep(formComponents[1])
+      
+        // console.log({ name, email, message, currentStep })
+    }
+
+
     return (
         <Container className='wrapper'>
             <h1 className='title'>Vamos conversar.</h1>
             <p className='contact-p'>Entre em contato preenchendo o formulário a seguir ou pelo email <a href="mailto:itsimoes1@gmail.com?subject=Olá!" class="links">itsimoes1@gmail.com</a>.</p>
             <div className='form-wrapper'>
-                <form id='contact-form' action='submit'>
-                    <label >Nome:</label>
-                    <input className='textField' type="text" placeholder='Digite seu nome'/>
-                    <label >Email:</label>
-                    <input className='textField' type="text" placeholder='Digite seu email'/>
-                    <label >Mensagem:</label>
-                    <textarea className='textField' type="text-area" placeholder='Digite sua mensagem'/>
-                    <button className='contact-button' type='submit' form='contact-form'value='Enviar'>Enviar</button>
+                <form id='contact-form' action='submit' onSubmit={(e) => changeStep(e)}>
+                    {currentStep}
+                    <button
+                        className='contact-button'
+                        type='submit'
+                        form='contact-form'
+                        value='Enviar'>Enviar
+                    </button>
                 </form>
             </div>
         </Container>
---
+++ src/components/Contact/UserForm.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useState } from 'react'
+import './style.css'
+
+
+
+const UserForm = () => {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+
+  // const handleSubmitForm = (e) => {
+  //   e.preventDefault()
+
+  //   console.log({ name, email, message })
+  // }
+
+  return (
+    <div className='form'>
+      <label >Nome:</label>
+      <input
+        className='textField'
+        type="text"
+        placeholder='Digite seu nome'
+        required
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <label >Email:</label>
+      <input
+        className='textField'
+        type="text"
+        placeholder='Digite seu email'
+        required
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <label >Mensagem:</label>
+      <textarea
+        className='textField'
+        type="text-area"
+        placeholder='Digite sua mensagem'
+        required
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />     
+    </div>
+  )
+}
+
+export default UserForm
