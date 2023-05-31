f2b3cfaf894fe99d1f67141895fd1622594655c9
Italo Simões <2d.italo@gmail.com>
2023-05-31 20:39:35 -0300

    update nos cards: adicionados botões e links das paginas dos projetos

--- src/components/Projetos/Projetos.jsx
+++ src/components/Projetos/Projetos.jsx
@@ -8,7 +8,7 @@ import Blog from "../../assets/blog.png";
 import Swotch from "../../assets/swotch.png";
 import Jbl from "../../assets/jbl.png";
 import ReactIcon from "../../assets/logo192.png";
-import { FlightLand } from "@mui/icons-material";
+import { FlightLand, GitHub, Lightbulb } from "@mui/icons-material";
 
 const Projetos = () => {
   return (
@@ -44,18 +44,60 @@ const Projetos = () => {
               >
                 <img className="thumb" src={Countdown} alt="" />
               </a>
-              <p>Countdown Timer</p>
+              <div>
+                <div className="projetos-subtitle-off">
+                  <Lightbulb className="projetos-icon-off" />
+                  <p>Countdown Timer</p>
+                </div>
+              </div>
+              <a
+                className="projetos-link"
+                href="https://github.com/itsimoes/countdown-timer"
+                target="_blank"
+                alt=""
+                rel="noreferrer"
+              >
+                <div className="projetos-subtitle">
+                  <GitHub className="projetos-icon-on" />
+                  <p>Veja no Github</p>
+                </div>
+              </a>
             </div>
+
             <div className="card">
               <a
-                href="https://github.com/itsimoes/pokedex"
+                className="projetos-link"
+                href="https://itsimoes.github.io/pokedex/"
                 target="_blank"
                 alt=""
                 rel="noreferrer"
               >
                 <img className="thumb" src={Pokedex} alt="" />
               </a>
-              <p>Pokedex</p>
+              <a
+                className="projetos-link"
+                href="https://itsimoes.github.io/pokedex/"
+                target="_blank"
+                alt=""
+                rel="noreferrer"
+              >
+                <div className="projetos-subtitle">
+                  <Lightbulb className="projetos-icon-on" />
+                  <p>Pokedex</p>
+                </div>
+              </a>
+              <a
+                className="projetos-link"
+                href="https://github.com/itsimoes/pokedex"
+                target="_blank"
+                alt=""
+                rel="noreferrer"
+              >
+                <div className="projetos-subtitle">
+                  <GitHub className="projetos-icon-on" />
+                  <p>Veja no Github</p>
+                </div>
+              </a>
             </div>
           </div>
 
@@ -66,36 +108,105 @@ const Projetos = () => {
           <div className="card-wrapper">
             <div className="card">
               <a
-                href="https://github.com/itsimoes/LP-blog-soon"
+                href="https://itsimoes.github.io/LP-blog-soon/"
                 target="_blank"
                 alt=""
                 rel="noreferrer"
               >
                 <img className="thumb" src={Blog} alt="" />
               </a>
-              <p>Travel Blog</p>
+              <a
+                className="projetos-link"
+                href="https://itsimoes.github.io/LP-blog-soon/"
+                target="_blank"
+                alt=""
+                rel="noreferrer"
+              >
+                <div className="projetos-subtitle">
+                  <Lightbulb className="projetos-icon-on" />
+                  <p>Travel Blog</p>
+                </div>
+              </a>
+              <a
+                className="projetos-link"
+                href="https://github.com/itsimoes/LP-blog-soon"
+                target="_blank"
+                alt=""
+                rel="noreferrer"
+              >
+                <div className="projetos-subtitle">
+                  <GitHub className="projetos-icon-on" />
+                  <p>Veja no Github</p>
+                </div>
+              </a>
             </div>
             <div className="card">
               <a
-                href="https://github.com/itsimoes/LP-tech-product"
+                href="https://itsimoes.github.io/LP-tech-product/"
                 target="_blank"
                 alt=""
                 rel="noreferrer"
               >
                 <img className="thumb" src={Jbl} alt="" />
               </a>
-              <p>Tech Product</p>
+              <a
+                className="projetos-link"
+                href="https://itsimoes.github.io/LP-tech-product/"
+                target="_blank"
+                alt=""
+                rel="noreferrer"
+              >
+                <div className="projetos-subtitle">
+                  <Lightbulb className="projetos-icon-on" />
+                  <p>Tech Product</p>
+                </div>
+              </a>
+              <a
+                className="projetos-link"
+                href="https://github.com/itsimoes/LP-tech-product"
+                target="_blank"
+                alt=""
+                rel="noreferrer"
+              >
+                <div className="projetos-subtitle">
+                  <GitHub className="projetos-icon-on" />
+                  <p>Veja no Github</p>
+                </div>
+              </a>
             </div>
             <div className="card">
               <a
-                href="https://github.com/itsimoes/LP-nontendo-swotch"
+                href="https://itsimoes.github.io/LP-nontendo-swotch/"
                 target="_blank"
                 alt=""
                 rel="noreferrer"
               >
                 <img className="thumb" src={Swotch} alt="" />
               </a>
-              <p>Videogame</p>
+              <a
+                className="projetos-link"
+                href="https://itsimoes.github.io/LP-nontendo-swotch/"
+                target="_blank"
+                alt=""
+                rel="noreferrer"
+              >
+                <div className="projetos-subtitle">
+                  <Lightbulb className="projetos-icon-on" />
+                  <p>Videogame</p>
+                </div>
+              </a>
+              <a
+                className="projetos-link"
+                href="https://github.com/itsimoes/LP-nontendo-swotch"
+                target="_blank"
+                alt=""
+                rel="noreferrer"
+              >
+                <div className="projetos-subtitle">
+                  <GitHub className="projetos-icon-on" />
+                  <p>Veja no Github</p>
+                </div>
+              </a>
             </div>
           </div>
         </div>
